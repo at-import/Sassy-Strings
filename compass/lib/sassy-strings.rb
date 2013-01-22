46902dc6b88feb8cc2b2cfe492dea108cc11697a
Sam Richard <snugug@gmail.com>
2013-01-22 10:48:24 -0500

    Added str-pos function. Returns -1 if string not found

--- compass/lib/sassy-strings.rb
+++ compass/lib/sassy-strings.rb
@@ -3,11 +3,13 @@ Compass::Frameworks.register("sassy-strings", :path => "#{File.dirname(__FILE__)
 
 # Sassy String Functions
 module Sass::Script::Functions
+  # Replace String
   def str_replace(needle, replace, haystack)
     result = haystack.value.to_s.gsub(needle.value.to_s, replace.value.to_s)
     Sass::Script::String.new(result)
   end
 
+  # Split String
   def split_string(string, key)
     items = string.value.split(key.value)
     if items.count == 1
@@ -16,11 +18,21 @@ module Sass::Script::Functions
       Sass::Script::List.new(items.map{|i| Sass::Script::String.new(i)}, :comma)
     end
   end
+
+  # String Position
+  def str_pos(needle, haystack)
+    if haystack.value.to_s.index(needle.value.to_s)
+      Sass::Script::Number.new(haystack.value.to_s.index(needle.value.to_s))
+    else
+      Sass::Script::Number.new(-1)
+    end
+  end
+
 end
 
 module SassyStrings
 
-  VERSION = "0.1.1"
-  DATE = "2013-01-21"
+  VERSION = "0.2.1"
+  DATE = "2013-01-22"
 
 end
